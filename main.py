9ff374c05353f913d40eb07fd4c3ef52fd64cbcc
w-winiarz <winiarzwojtek@gmail.com>
2024-06-01 12:44:02 +0200

    2

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-s = pd.Series([1, 3, 5, np.nan, 6, 8])
-print(s)
-s = pd.Series([10, 12, 8, 14], index=['a', 'b', 'c', 'd'])
-print(s)
-data = {'Kraj': ['Belgia', 'Indie', 'Brazylia'],
-      'Stolica': ['Bruksela', 'New Delhi', 'Brasilia'],
-      'Populacja': [11190846, 1303171035, 207847528]}
-df = pd.DataFrame(data)
+# s = pd.Series([1, 3, 5, np.nan, 6, 8])
+# print(s)
+# s = pd.Series([10, 12, 8, 14], index=['a', 'b', 'c', 'd'])
+# print(s)
+# data = {'Kraj': ['Belgia', 'Indie', 'Brazylia'],
+#       'Stolica': ['Bruksela', 'New Delhi', 'Brasilia'],
+#       'Populacja': [11190846, 1303171035, 207847528]}
+# df = pd.DataFrame(data)
 # print(df)
 # print(df.dtypes)
 #
@@ -99,8 +99,68 @@ df = pd.DataFrame(data)
 
 import matplotlib.pyplot as plt
 
-ts= pd.Series(np.random.randn(1000))
+# ts= pd.Series(np.random.randn(1000))
+# ts=ts.cumsum()
+# print(ts)
+# ts.plot()
+# plt.show()
+
+
+s = pd.Series([1, 3, 5, np.nan, 6, 8])
+print(s)
+s = pd.Series([10, 12, 8, 14], index=['a', 'b', 'c', 'd'])
+print(s)
+data = {'Kraj': ['Belgia', 'Indie', 'Brazylia'],
+      'Stolica': ['Bruksela', 'New Delhi', 'Brasilia'],
+      'Populacja': [11190846, 1303171035, 207847528]}
+df = pd.DataFrame(data)
+
+df.loc[4]=['Polska', 'Warszawa', 38675467]
+
+df.drop("Kraj", axis=1, inplace=True)
+print(df)
+df['Kontynent']=['Europa', 'Azja',
+                 'Ameryka Południowa', 'Europa']
+
+
+
+
+
+grupa = df.groupby(['Kontynent']).agg({'Populacja':['sum']})
+print(grupa)
+
+grupa.plot(kind='bar', xlabel='Kontynent',ylabel='Mld',rot=0,
+           legend=True,
+           title='Populacja z podziałem na kontynenty')
+wykres=grupa.plot.bar()
+wykres.set_ylabel("Mld")
+wykres.set_xlabel('Kontynent')
+wykres.tick_params(axis='x',labelrotation=0)
+wykres.legend()
+wykres.set_title('Populacja z podziałem na kontynenty')
+# plt.xticks(rotation=0)
+plt.savefig('wykres.png')
+plt.show()
+
+
+df=pd.read_csv('dane.csv', header=0, sep=";",
+               decimal=".")
+print(df)
+grupa = (df.groupby(['Imię i nazwisko'])
+         .agg({'Wartość zamówienia':["sum"]}))
+grupa.plot(kind='pie', subplots=True, autopct='%.2f %%',
+           fontsize=20, figsize=(6,6), colors=['red','green'])
+# wykres=grupa.plot.pie(subplots=True,autopct='%.2f %%',
+#                       fontsize=20, figsize=(6,6))
+plt.legend(loc="lower right")
+plt.title('Suma zamówienia dla sprzedawcy')
+plt.show()
+
+ts = pd.Series(np.random.randn(1000))
 ts=ts.cumsum()
-print(ts)
-ts.plot()
+df = pd.DataFrame(ts, columns=['wartości'])
+print(df)
+df['Średnia krocząca'] = df.rolling(window=20).mean()
+df.plot()
+plt.legend()
 plt.show()
